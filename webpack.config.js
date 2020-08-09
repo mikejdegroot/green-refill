e2670a5c63ae7c2d3eade731426ae344c79a4559
Mike de Groot - Remote <mike@entwurf.co.uk>
2020-08-09 11:25:36 +0100

    webpack serving

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,65 @@
 const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './index.js',
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/index.html', to: '' }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'styles.css',
+      // chunkFilename: '[id].css'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    new BrowserSyncPlugin({
+      // browse to http://localhost:3000/ during development,
+      // ./public directory is being served
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: ['dist'] }
+    })
+  ],
   module: {
     rules: [
+      // {
+      //   test: /\.js$/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: { presets: ['es2015'] }
+      //   }
+      // },
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
+          test: /\.scss$/,
+          use: [
+              'style-loader',
+              MiniCssExtractPlugin.loader,
+              {
+                  loader: "css-loader",
+                  options: {
+                      // minimize: true,
+                      // sourceMap: true
+                  }
+              },
+              {
+                  loader: "sass-loader"
+              }
+          ]
+      },
     ]
   }
 };
